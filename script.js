266bda30aaf84964a05fa28e48881d0fa3f22a27
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-19 15:50:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function findDOMLevel() {
+  let element = document.getElementById('level');
+  let level = 0;
+
+  while (element.parentNode) {
+    element = element.parentNode;
+    level++;
+  }
+
+  alert("The level of the element is: " + level);
+}
+
+findDOMLevel();
